f778ed79cfbaaf07985d848bc66b82e5587cc9fb
Ducld01 <ducld01.dev@gmail.com>
2021-10-07 16:25:36 +0700

    Jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,42 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
     function openmenumobile() {
-        $('.open-moblie-menu').click(function() {
+        $('.open-moblie-menu').click(function () {
             $('.menu-tab-mobile').addClass('active');
             $('.bg-menu-mobile').addClass('active');
         })
-        $('.tab-footer').click(function() {
+        $('.tab-footer').click(function () {
             $('.menu-tab-mobile').removeClass('active');
             $('.bg-menu-mobile').removeClass('active');
         })
-        $('.bg-menu-mobile').click(function() {
+        $('.bg-menu-mobile').click(function () {
             $('.menu-tab-mobile').removeClass('active');
             $('.bg-menu-mobile').removeClass('active');
         })
     }
     function activeMenu() {
-        $('.tab-nav').click(function() {
+        $('.tab-nav').click(function () {
             $('.tab-account-mobile').hide();
             $('.tab-menu-mobile').show();
             $('.tab-nav').addClass('clicked');
             $('.tab-login').removeClass('clicked');
         })
-        $('.tab-login').click(function() {
+        $('.tab-login').click(function () {
             $('.tab-account-mobile').show();
             $('.tab-login').addClass('clicked');
             $('.tab-nav').removeClass('clicked');
             $('.tab-menu-mobile').hide();
         })
-        $('.reset-password').click(function() {
+        $('.reset-password').click(function () {
             $('.login-form-content').hide();
             $('.recover-password').show();
             $('.form-register').hide();
         })
-        $('.link-to-login').click(function() {
+        $('.link-to-login').click(function () {
             $('.login-form-content').show();
             $('.recover-password').hide();
             $('.form-register').hide();
         })
-        $('.btn-to-register').click(function() {
+        $('.btn-to-register').click(function () {
             $('.login-form-content').hide();
             $('.recover-password').hide();
             $('.form-register').show();
@@ -47,8 +47,10 @@ jQuery(document).ready(function($){
             infinite: true,
             slidesToShow: 1,
             dots: true,
-            adaptiveHeight:true,
+            adaptiveHeight: true,
             slidesToScroll: 1,
+            // autoplay: true,
+            // autoplaySpeed: 2000,
         })
     }
     openmenumobile();
